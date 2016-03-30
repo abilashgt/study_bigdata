286727f8b56397b7279d6f1adb44d1a9730d9020
Abilash Thomas <abilash4work@gmail.com>
2016-03-30 19:36:28 +0530

    flightlog current progress

---
+++ hadoop/tcs/flightlog/FlightLog.java
@@ -0,0 +1,52 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+
+/**
+ * 
+ */
+
+/**
+ * @author 984620
+ *
+ */
+public class FlightLog {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+
+		Job job = new Job(conf, "flightlog");
+
+		// input
+		job.setInputFormatClass(TextInputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		
+		// output
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		// mapper
+		job.setMapperClass(FlightMap.class);
+		
+		// reducer
+		job.setReducerClass(FlightReduce.class);
+		job.setNumReduceTasks(0);
+		
+		// run
+		job.waitForCompletion(true);
+
+	}
+
+}
