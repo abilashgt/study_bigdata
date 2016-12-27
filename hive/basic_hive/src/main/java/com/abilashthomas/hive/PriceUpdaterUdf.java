cb9059ff8c640d72b8b740b597d3264aa2271232
Abhilash Thomas <Abhilash.Z.Thomas.ap@nielsen.com>
2016-12-27 18:04:09 +0530

    hive udf example update

--- hive/basic_hive/src/main/java/com/abilashthomas/hive/PriceUpdaterUdf.java
+++ hive/basic_hive/src/main/java/com/abilashthomas/hive/PriceUpdaterUdf.java
@@ -2,10 +2,7 @@ package com.abilashthomas.hive;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 
-/**
- * Created by Abilash George Thomas on 12/27/2016.
- * Email: abhilash.z.thomas.ap@nielsen.com
- */
+
 public class PriceUpdaterUdf extends UDF {
     public double evaluate(double price, double multiplier){
         return price*multiplier;
