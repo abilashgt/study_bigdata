f244df52e98fdb077aa58abf429d95e4ec81f0b7
Abilash George Thomas <984620@01hw898734.tcs-mobility.com>
2017-01-25 19:27:39 +0530

    kafka started

---
+++ spark/basic_spark/scripts/kafka.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+spark-submit \
+--num-executors 80 --executor-cores 4 --executor-memory 16g --driver-memory 4g --master yarn --conf spark.yarn.executor.memoryOverhead=4000 \
+--packages org.apache.spark:spark-streaming-kafka-0-8_2.10:2.1.0 \
+--class kafka.ConsumerExample \
+../target/basic-spark.jar \
+localhost:2181 Hello-Kafka Hello-Kafka 2
