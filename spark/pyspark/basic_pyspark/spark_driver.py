e069709eb65dbad192db0a6a308b495c7b22ffca
Abilash George Thomas <abilashgt@users.noreply.github.com>
2019-01-23 02:49:31 -0800

    pyspark dataframe untested

--- spark/pyspark/basic_pyspark/spark_driver.py
+++ spark/pyspark/basic_pyspark/spark_driver.py
@@ -7,6 +7,7 @@ from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from pyspark.sql import Row
 
 sc = SparkContext('local', 'Exam_3')
 sqlc = SQLContext(sc)
@@ -14,11 +15,17 @@ sqlc = SQLContext(sc)
 # test python
 print("test python")
 
-# test spark
+# test spark create rdd
 rdd = sc.parallelize(['test','spark'])
 print(rdd.collect())  # print
 
-# test rdd
+# create dataframe
+l = [('Abilash',25),('George',22),('Thomas',20),('Mathew',26)]
+rdd = sc.parallelize(l)
+people = rdd.map(lambda x: Row(name=x[0], age=int(x[1])))
+people_noschema = rdd.map(lambda x: Row(x[0], int(x[1])))
+sqlContext.createDataFrame(people).show() # print
+sqlContext.createDataFrame(people_noschema).show() # print
 
 
 sys.exit()
